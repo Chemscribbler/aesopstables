469bcb9518995d10394b97b1c67125452fd9e668
Chemscribbler <jeffgp93@gmail.com>
2023-04-16 19:56:25 -0400

    creating Heroku procfiles

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn aesopstables:app
--- config.py
+++ config.py
@@ -4,8 +4,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "").replace(
+        "postgres://", "postgresql://"
     ) or "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get("SECRET_KEY") or "a-secret-key"
+    LOG_TO_STDOUT = os.environ.get("LOG_TO_STDOUT")
