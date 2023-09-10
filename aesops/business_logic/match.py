cd8b227e84be7ccbf17af2412be1ada91ef5e587
Chemscribbler <jeffgp93@gmail.com>
2023-09-10 10:12:29 -0400

    fix Value to value typo

--- aesops/business_logic/match.py
+++ aesops/business_logic/match.py
@@ -3,40 +3,52 @@ from data_models.match import Match, MatchResult
 from data_models.model_store import db
 from . import players as p_logic
 
-def conclude(match :Match):
-    if match.result not in [MatchResult.CORP_WIN.Value, MatchResult.RUNNER_WIN.Value, MatchResult.DRAW.Value, MatchResult.INTENTIONAL_DRAW.Value]:
+
+def conclude(match: Match):
+    if match.result not in [
+        MatchResult.CORP_WIN.value,
+        MatchResult.RUNNER_WIN.value,
+        MatchResult.DRAW.value,
+        MatchResult.INTENTIONAL_DRAW.value,
+    ]:
         raise ConclusionError("No result recorded")
     match.concluded = True
     db.session.add(match)
     db.session.commit()
 
-def corp_win(match :Match):
+
+def corp_win(match: Match):
     match.result = MatchResult.CORP_WIN.value
     db.session.add(match)
     db.session.commit()
 
-def runner_win(match :Match):
+
+def runner_win(match: Match):
     match.result = MatchResult.RUNNER_WIN.value
     db.session.add(match)
     db.session.commit()
 
-def tie(match :Match):
+
+def tie(match: Match):
     match.result = MatchResult.DRAW.value
     db.session.add(match)
     db.session.commit()
 
-def intentional_draw(match :Match):
+
+def intentional_draw(match: Match):
     match.result = MatchResult.INTENTIONAL_DRAW.value
     db.session.add(match)
     db.session.commit()
 
-def reset(match :Match):
+
+def reset(match: Match):
     match.result = None
     match.concluded = False
     db.session.add(match)
     db.session.commit()
 
-def delete(match :Match):
+
+def delete(match: Match):
     if match.is_bye:
         match.corp_player.recieved_bye = False
         p_logic.reset(match.corp_player)
